eb4f086a6727774c054fe4324e597031820395b1
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2024-08-08 21:05:56 -0400

    Create runtests.jl

---
+++ test/runtests.jl
@@ -0,0 +1,15 @@
+using BaseMicroFloats, Test
+
+BF32 = BaseMicroFloat(3, 2);
+
+@test typeof(encoding(BF32)[1]) == UInt8
+@test typeof(values(BF32)[1]) == Float32
+
+@test encoding(BF32) == [0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07]
+@test values(BF32) == [0.0, 0.25, 0.5, 0.75, 1.0, 1.5, 2.0, 3.0]
+
+BF98 = BaseMicroFloat(9, 8);
+
+@test typeof(encoding(BF98)[1]) == UInt16
+@test typeof(values(BF98)[1]) == Float64
+
