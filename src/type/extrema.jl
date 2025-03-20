d792ebbe4599a4227f63a88ea131b798177228d1
Jeffrey Sarnoff <jeffrey.sarnoff@gmail.com>
2025-03-20 10:11:55 -0400

    fix

--- src/type/extrema.jl
+++ src/type/extrema.jl
@@ -5,7 +5,7 @@ Base.exponent_bias(::Type{T}) where {T<:AbstractAIFloat} = 2^(nExpBits(T) - 1) -
 exponent_max(::Type{T}) where {T<:AbstractAIFloat} = 2.0^(biased_exponent_max(T))
 exponent_min(::Type{T}) where {T<:AbstractAIFloat} = 2.0^(biased_exponent_min(T))
 
-subnormal_max(::Type{T}) where {T<:AbstractAIFloat} = exponent_min(T) * (1//nFracValues(x))
+subnormal_max(::Type{T}) where {T<:AbstractAIFloat} = exponent_min(T) * (1//nFracValues(T))
 subnormal_min(::Type{T}) where {T<:AbstractAIFloat} = exponent_min(T) * ((nFracValues(T) - 1)//nFracValues(T))
 
 normal_max(::Type{T}) where {T<:AbstractAIFloat} = exponent_max(T) * ((nFracValues(T) - 1)//nFracValues(T))
