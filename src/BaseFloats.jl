28d9ed028d17695ba42ba1c9c8848667d1288cb7
Jeffrey Sarnoff <jeffrey.sarnoff@gmail.com>
2025-03-23 11:53:16 -0400

    prep

--- src/BaseFloats.jl
+++ src/BaseFloats.jl
@@ -7,8 +7,54 @@ export AbstractMicroFloat, AkoBaseFloat,
 
 import Base: precision, values
 
-abstract type AbstractMicroFloat{Bitwidth, Precision}  <: AbstractFloat end
-abstract type AkoBaseMicroFloat{Bitwidth, Precision} <: AbstractMicroFloat{Bitwidth, Precision} end
+"""
+    AbstractFloatML{Bitwidth, Precision}
+
+our extended family of floating-point types for machine learning
+
+- `Bitwidth` is the number of bits in the encoding
+- `Precision` is the number of bits in the significand
+   (includes the implicit bit)
+""" AbstractFloatML
+
+"""
+    AbsSFloatML{Bitwidth, Precision}
+    - AbstractSignedFloatML
+
+our kinds of signed floating-point types for machine learning
+
+- `Bitwidth` is the number of bits in the encoding
+- `Precision` is the number of bits in the significand
+   (includes the implicit bit)
+""" AbsSFloatML
+
+"""
+    AbsUFloatML{Bitwidth, Precision}
+    - AbstractUnsignedFloatML
+
+our kinds of unsigned floating-point types for machine learning
+
+- `Bitwidth` is the number of bits in the encoding
+- `Precision` is the number of bits in the significand
+   (includes the implicit bit)
+""" AbsUFloatML
+
+"""
+    AbstractFloatML{Bitwidth, Precision}
+
+our extended family of floating-point types for machine learning
+
+- `Bitwidth` is the number of bits in the encoding
+- `Precision` is the number of bits in the significand
+   (includes the implicit bit)
+""" AbstractFloatML
+
+# our extended family of floating-point types for machine learning
+abstract type AbstractFloatML{Bitwidth, Precision}  <: AbstractFloat end
+# some are Signed, having positive and negative values (and 0, NaN)
+abstract type AbsSFloatML{B,P} <: AbstractMicroFloat{Bitwidth, Precision} end
+# some are Unsigned, having non-negative values only (positives and 0, NaN)
+abstract type AbsUFloatML{B,P} <: AbstractMicroFloat{Bitwidth, Precision} end
 
 include("config.jl")
 include("construct.jl")
--- src/config.jl
+++ src/config.jl
@@ -1,28 +1,48 @@
-abstract type MicroFloatConfig end
+abstract type FloatMLConfig{Bitwidth, Precision, IsSigned} end
 
-Base.@kwdef struct Config{SignBits, Bits, Precision} <: MicroFloatConfig
-    n_bits::Int = Bits
+Base.@kwdef struct FloatMLconfig{Bitwidth, Precision, IsSigned} <: FloatMLConfig{Bitwidth, Precision, IsSigned}
+    n_nans::Int = 1
+    n_zeros::Int = 1
+
+    n_bits::Int = Bitwidth
     n_significant_bits::Int = Precision
-    n_fraction_bits::Int = Precision - 1
-    n_exponent_bits = Bits - Precision + iszero(SignBits)
-    n_sign_bits::Int = SignBits
+    n_fraction_bits::Int = Precision - 1            # trailing (explict) significand bits
+    n_exponent_bits::Int = Bitwidth - Precision 
+    n_sign_bits::Int = 0 + IsSigned
 
     n_values::Int = 2^n_bits
-    n_fractions::Int = 2^n_fraction_bits
-    n_exponents::Int = 2^n_exponent_bits
-    n_normal_exponents::Int = n_exponents - 1
+    n_fraction_values::Int = 2^n_fraction_bits
+    n_exponent_values::Int = 2^n_exponent_bits
 
-    n_fraction_cycles::Int = n_exponents
-    n_exponent_cycles::Int = n_fractions
+    n_fraction_cycles::Int = n_exponent_values      # structural, generative
+    n_exponent_cycles::Int = n_fraction_values      # structural, generative
 
-    exponent_max::Int = 2^(Bits-Precision) - 1
-    exponent_min::Int = 1 - 2^(Bits-Precision) 
-    exponent_bias::Int = 2^(Bits-Precision)
+    exp_bias::Int = 2^(n_exponent_bits - 1) - 1
+
+    unbiased_exponent_min::Int = 0 - exp_bias
+    unbiased_exponent_max::Int = (n_exponent_values - 1) - exp_bias
+
+    exponent_min::Float64 = 2.0^unbiased_exponent_min
+    exponent_max::Float64 = 2.0^unbiased_exponent_max
 end
 
-const ConfigFields = fieldnames(Config)
-const ConfigTypes  = NTuple{length(ConfigFields), Int}
-const ConfigNT = NamedTuple{ConfigFields, ConfigTypes}
+const ConfigFloatMLnames = fieldnames(FloatMLconfig)
+const ConfigFloatMLtypes = Tuple{fieldtypes(FloatMLconfig)...}
+const ConfigFloatML = NamedTuple{ConfigFloatMLnames, ConfigFloatMLtypes}
+
+function config_floatml(bitwidth, precision, issigned)
+    specs = FloatMLconfig{bitwidth, precision, issigned}()
+
+    # biased_exponents = collect( (0:n_exponents-1) .- bias )
+    # exponent for subnormals equals the minimum exponent for normals
+    # biased_exponents[1] = biased_exponents[2]
+    ConfigFloatML((getfield(specs, i) for i in 1:nfields(specs)))
+end
+
+
+
+
+
 
 UnsignedConfig(Bits, Precision) = Config{0, Bits, Precision}()
 SignedConfig(Bits, Precision) = Config{1, Bits, Precision}()
