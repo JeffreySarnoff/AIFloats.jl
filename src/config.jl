54caa72ae6926c262211223ebef41a7d47a112ef
Jeffrey Sarnoff <jeffrey.sarnoff@gmail.com>
2025-03-23 12:41:41 -0400

    fix case precision=1

--- src/config.jl
+++ src/config.jl
@@ -11,17 +11,21 @@ Base.@kwdef struct FloatMLconfig{Bitwidth, Precision, IsSigned, IsExtended} <: F
     n_exponent_bits::Int = Bitwidth - Precision 
     n_sign_bits::Int = 0 + IsSigned
 
+    # n_<>_values counts the number of signed occurrences 
+
     n_values::Int = 2^n_bits
-    n_extended_values::Int = n_values - n_nans      # without NaN
-    n_finite_values::Int = n_extended_values - n_infs
+    n_extended_values::Int = n_values - n_nans                  # without NaN
+    n_finite_values::Int = n_extended_values - n_infs           # without NaN and Infinity 
     n_nonzero_finite_values::Int = n_finite_values - n_zeros
 
-    n_fraction_values::Int = 2^n_fraction_bits
+    n_fraction_values::Int = 2^n_fraction_bits * (1 + IsSigned)
     n_exponent_values::Int = 2^n_exponent_bits
 
-    n_subnormal_values::Int = n_fraction_values = n_fraction_values - 1
-    n_normal_values::Int = n_values - n_subnormal_values
-
+    n_subnormal_values::Int = (n_significant_bits > 1) ? n_fraction_values : 0
+    n_normal_values::Int = n_finite_values - n_subnormal_values
+    
+    # n_<>_magnitudes counts the number of non-negative occurrences 
+    
     n_magnitudes::Int = IsSigned ? n_nonzero_finite_values >> 1 : n_nonzero_finite_values
     n_subnormal_magnitudes::Int = IsSigned ? n_subnormal_values >> 1 : n_subnormal_values
     n_normal_magnitudes::Int = IsSigned ? n_normal_values >> 1 : n_normal_values
@@ -31,7 +35,7 @@ Base.@kwdef struct FloatMLconfig{Bitwidth, Precision, IsSigned, IsExtended} <: F
 
     exp_bias::Int = 2^(n_exponent_bits - 1) - 1
 
-    unbiased_exponent_min::Int = (n_significant_bits >1) ? -exp_bias : -exp_bias + 1
+    unbiased_exponent_min::Int = (n_significant_bits > 1) ? -exp_bias : -exp_bias + 1
     unbiased_exponent_max::Int = exp_bias
 
     exponent_min::Float64 = 2.0^unbiased_exponent_min
