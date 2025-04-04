fbb15ede6e614d0b033625cff882ae8d121d5bc2
Jeffrey Sarnoff <jeffrey.sarnoff@gmail.com>
2025-04-04 07:55:55 -0400

    docs make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -14,8 +14,25 @@ makedocs(;
     authors = "Jeffrey Sarnoff <jeffrey.sarnoff@gmail.com>",
     repo = "https://github.com/DiademSpecialProjects/FloatsForML.jl/blob/{commit}{path}#{line}",
     sitename = "FloatsForML.jl",
-    format = Documenter.HTML(; canonical = "https://DiademSpecialProjects.github.io/FloatsForML.jl"),
-    pages = ["index.md"; numbered_pages],
-)
-
-deploydocs(; repo = "github.com/DiademSpecialProjects/FloatsForML.jl")
+    format = Documenter.HTML(; 
+                 repolink="https://github.com/DiademSpecialProjects/FloatsForML.jl/",
+                 prettyurls=get(ENV, "CI", "false") == "true",
+                 canonical = "https://DiademSpecialProjects.github.io/FloatsForML.jl",
+                 edit_link  = "main",
+                 assets=String[],
+            ),
+    pages = [
+                 "Home" => "index.md",
+                 "Reference" => "reference.md";
+                 numbered_pages
+            ],
+   )
+ 
+deploydocs(; repo = "github.com/DiademSpecialProjects/FloatsForML.jl",
+             target = "FloatsForML.jl",
+             push_preview = true,
+             devbranch = "main",
+             force = true,
+             commit_message = "Update documentation",
+             branch = "gh-pages",
+          )
