b6cc64d134b95d7a7e35202249a3b239d333db83
Jeffrey Sarnoff <jeffrey.sarnoff@gmail.com>
2025-04-10 08:06:44 -0400

    rename

--- src/type/collective.jl
+++ src/type/collective.jl
@@ -1,21 +1,21 @@
-struct UFiniteFloatsML{Bits, SigBits, Floats, Codes} <: AbsUFiniteFloatML{Bits, SigBits}
-    floats::DenseVector{Floats}
-    codes::DenseVector{Codes}
+struct UFiniteFloatsML{Bits, SigBits, Float, Code} <: AbsUFiniteFloatML{Bits, SigBits}
+    floats::DenseVector{Float}
+    codes::DenseVector{Code}
 end
 
-struct UExtendedFloatsML{Bits, SigBits, Floats, Codes} <: AbsUExtendedFloatML{Bits, SigBits}
-    floats::DenseVector{Floats}
-    codes::DenseVector{Codes}
+struct UExtendedFloatsML{Bits, SigBits, Float, Code} <: AbsUExtendedFloatML{Bits, SigBits}
+    floats::DenseVector{Float}
+    codes::DenseVector{Code}
 end
 
-struct SFiniteFloatsML{Bits, SigBits, Floats, Codes} <: AbsSFiniteFloatML{Bits, SigBits}
-    floats::DenseVector{Floats}
-    codes::DenseVector{Codes}
+struct SFiniteFloatsML{Bits, SigBits, Float, Code} <: AbsSFiniteFloatML{Bits, SigBits}
+    floats::DenseVector{Float}
+    codes::DenseVector{Code}
 end
 
-struct SExtendedFloatsML{Bits, SigBits, Floats, Codes} <: AbsSExtendedFloatML{Bits, SigBits}
-    floats::DenseVector{Floats}
-    codes::DenseVector{Codes}
+struct SExtendedFloatsML{Bits, SigBits, Float, Code} <: AbsSExtendedFloatML{Bits, SigBits}
+    floats::DenseVector{Float}
+    codes::DenseVector{Code}
 end
 
 codes(@nospecialize(x::AbstractFloatML)) = x.codes
