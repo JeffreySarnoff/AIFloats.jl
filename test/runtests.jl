f4879175817db0fa6ffa9a8147cf39d6a3595912
Jeffrey Sarnoff <jeffrey.sarnoff@gmail.com>
2025-05-17 05:59:48 -0400

    tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,13 +1,13 @@
 using AlignedAllocs, FloatsForML, Test
 # AbstractAIFloat{K,P}
-include(s"C:\github\FloatsForML.jl\test\test_abstypes\test_inits.jl")
-include(s"C:\github\FloatsForML.jl\test\test_abstypes\test_abstract.jl")
+include("test_abstypes/test_inits.jl")
+include("test_abstypes/test_abstract.jl")
 
-include(s"C:\github\FloatsForML.jl\test\test_abstypes\test_absuais.jl")
-include(s"C:\github\FloatsForML.jl\test\test_abstypes\test_abssais.jl")
+include("test_abstypes/test_absuais.jl")
+include("test_abstypes/test_abssais.jl")
 
-include(s"C:\github\FloatsForML.jl\test\test_abstypes\test_absufais.jl")
-include(s"C:\github\FloatsForML.jl\test\test_abstypes\test_abssfais.jl")
+include("test_abstypes/test_absufais.jl")
+include("test_abstypes/test_abssfais.jl")
 
-include(s"C:\github\FloatsForML.jl\test\test_abstypes\test_absueais.jl")
-include(s"C:\github\FloatsForML.jl\test\test_abstypes\test_absseais.jl")
+include("test_abstypes/test_absueais.jl")
+include("test_abstypes/test_absseais.jl")
