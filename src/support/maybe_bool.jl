eb5ea0866a25f4982da1e2a884d852fd9803e075
Jeffrey Sarnoff <jeffrey.sarnoff@gmail.com>
2025-06-29 07:54:56 -0400

    Some

--- src/support/maybe_bool.jl
+++ src/support/maybe_bool.jl
@@ -8,5 +8,12 @@ differ(x::Bool, y::Nothing)    = true
 differ(x::Nothing, y::Bool)    = true
 differ(x::Nothing, y::Nothing) = true               # this relation is necessary
 
+differ(x::Bool, y::Some{Nothing})    = true
+differ(x::Some{Nothing}, y::Bool)    = true
+differ(x::Nothing, y::Some{Nothing}) = true               # this relation is necessary
+
 Base.convert(::Type{Bool}, x::Nothing) = false      # the conversion logic promulgates consistency
 Base.convert(::Type{Nothing}, x::Bool) = nothing    # 
+
+Base.convert(::Type{Bool}, x::Some{Nothing}) = false            # the conversion logic promulgates consistency
+Base.convert(::Type{Some{Nothing}}, x::Bool) = Some(nothing)    # 
