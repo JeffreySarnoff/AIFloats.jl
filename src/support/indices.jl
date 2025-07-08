5b33df63fc7b022e78030f37faeed0ae11164d97
Jeffrey Sarnoff <jeffrey.sarnoff@gmail.com>
2025-07-08 18:27:39 -0400

    fix

--- src/support/indices.jl
+++ src/support/indices.jl
@@ -58,8 +58,6 @@ convert the P3019 encoding to a (0-based) offset
 
 """ code_to_offset
 
-
-
 """
     code_to_value(values, code)
 
@@ -67,7 +65,12 @@ convert the P3019 encoding to a (0-based) offset
                      else NaN
 """ code_to_value
 
-offset_to_value = code_to_value
+"""
+    offset_to_value(values, offset)
+
+    - values[offset+1] i f   0 <= code < length(values)
+                     else NaN
+""" offset_to_value
 
 """
     index_to_value(values, idx)
@@ -87,27 +90,64 @@ offset_to_value = code_to_value
 
 """ value_to_offset
 
-
 """
     value_to_index
 
 """ value_to_index
 
+code_to_value(xs, code) = xs[code_to_index(code)]
 
-value_to_index = 1 + value_to_offset
+value_to_code(xs, value) = index_to_code(value_to_index(xs, value))
 
+offset_to_value(xs, offset) = code_to_value(xs, offset)
 
-@inline function index_to_offset(x::StaticInt{N}) where {N}
-    (x % UInt16) - one(UInt16)
+value_to_offset(xs, value) = value_to_code(xs, value)
+
+index_to_value(xs, idx) = xs[idx]
+
+function value_to_index(aif::T, x::F) where {T<:AbstractAIFloat, F<:AbstractFloat}
+    idx = findfirst(isequal(x), floats(aif))
+    isnothing(idx) && throw(DomainError(x, "value_to_index: value $x not found in $aif"))
+    idx
 end
 
-@inline index_to_offset(x::U) where {U<:Unsigned} = x - one(UInt16)
-@inline index_to_offset(x::I) where {I<:Integer} = index_to_offset(x % UInt16)
+@inline function value_to_index(xs::T, x::F) where {T<:Vector{<:AbstractFloat}, F<:AbstractFloat}
+    idx = findfirst(isequal(x), xs)
+    isnothing(idx) && throw(DomainError(x, "value_to_index: value $x not found in $xs"))
+    idx
+end
 
-@inline index_to_offset(::Type{Target}, x::T) where {Target<:Integer, T<:Integer} = (x - one(T)) % Target
+code_to_index(xs::Vector{T}, code::U) where {T<:AbstractFloat, U<:Unsigned} =
+    offset_to_index(xs, code_to_offset(xs, code))
 
-@inline function index_to_offset(xs::T, x::I) where {T<:AbstractAIFloat, I<:Integer}
-    !(0x01 <= x <= length(floats(xs))) || throw(DomainError(x, "index_to_offset: index $x out of bounds for $T"))
+code_to_index(code::U) where {U<:Unsigned} = code + 0x01
+
+index_to_code(xs::T, index::I) where {T<:Vector{<:AbstractFloat}, I<:Integer} =
+    offset_to_code(xs, index_to_offset(index))
+
+index_to_code(idx::I) where {I<:Integer} = idx - 0x01
+
+code_to_offset(xs::Vector{T}, code::U) where {T<:AbstractFloat, U<:Unsigned} = code
+
+code_to_offset(code::U) where {U<:Unsigned} = code
+
+offset_to_code(xs::Vector{T}, offset::U) where {T<:AbstractFloat, U<:Unsigned} = offset
+offset_to_code(xs::Vector{T}, offset::I) where {T<:AbstractFloat, I<:Integer} = offset % UInt16
+
+offset_to_code(offset::I) where {I<:Integer} = offset % UInt16
+
+index_to_offset(x::U) where {U<:Unsigned} = x - one(UInt16)
+index_to_offset(x::I) where {I<:Integer} = index_to_offset(x % UInt16)
+index_to_offset(x::StaticInt{N}) where {N} = (x % UInt16) - one(UInt16)
+
+offset_to_index(x::U) where {U<:Unsigned} = x + one(UInt16)
+offset_to_index(x::I) where {I<:Integer} = x + one(UInt16)
+offset_to_index(x::StaticInt{N}) where {N} = (x  % UInt16) + one(UInt16)
+
+index_to_offset(::Type{Target}, x::T) where {Target<:Integer, T<:Integer} = (x - one(T)) % Target
+
+@inline function index_to_offset(aif::T, x::I) where {T<:AbstractAIFloat, I<:Integer}
+    !(0x01 <= x <= length(floats(aif))) || throw(DomainError(x, "index_to_offset: index $x out of bounds for $T"))
     index_to_offset(x)
 end
 
@@ -116,17 +156,10 @@ end
     index_to_offset(x)
 end
 
-@inline function offset_to_index(x::StaticInt{N}) where {N}
-    x % UInt16 + one(UInt16)
-end
-
-@inline offset_to_index(x::U) where {U<:Unsigned} = x + one(UInt16)
-@inline offset_to_index(x::I) where {I<:Integer} = offset_to_index(x % UInt16)
-
 @inline offset_to_index(::Type{Target}, x::T) where {Target<:Integer, T<:Integer} = (x + one(T)) % Target
 
-@inline function offset_to_index(xs::T, x::I) where {T<:AbstractAIFloat, I<:Integer}
-    !(0x01 <= x <= length(floats(xs))) || throw(DomainError(x, "index_to_offset: index $x out of bounds for $T"))
+@inline function offset_to_index(aif::T, x::I) where {T<:AbstractAIFloat, I<:Integer}
+    !(0x01 <= x <= length(floats(aif))) || throw(DomainError(x, "index_to_offset: index $x out of bounds for $T"))
     offset_to_index(x)
 end
 
@@ -134,14 +167,10 @@ end
     !(0x01 <= x <= length(xs)) || throw(DomainError(x, "index_to_offset: index $x out of bounds for $T"))
     offset_to_index(x)
 end
-
-
-
+ 
 @inline function index_to_code(bits, index)
     index_to_offset(index) % typeforcode(bits)
 end
-
-
 
 @inline index1(::Type{T}) where {T<:AbstractUnsigned} = nValues(T) >> 0x0001 + 0x01
 @inline index1(x::T) where {T<:AbstractUnsigned} = index1(T)                                                            
@@ -149,23 +178,11 @@ end
 @inline index1(::Type{T}) where {T<:AbstractSigned} = nValues(T) >> 0x02 + 0x01
 @inline index1(x::T) where {T<:AbstractSigned} = index1(T)
 
-@inline function value_to_index(xs::T, x::F) where {T<:AbstractAIFloat, F<:AbstractFloat}
-    idx = findfirst(isequal(x), floats(xs))
-    isnothing(idx) && throw(DomainError(x, "value_to_index: value $x not found in $xs"))
-    idx
-end
-
-@inline function value_to_index(xs::T, x::F) where {T<:Vector{<:AbstractFloat}, F<:AbstractFloat}
-    idx = findfirst(isequal(x), xs)
-    isnothing(idx) && throw(DomainError(x, "value_to_index: value $x not found in $xs"))
-    idx
- end
-
-@inline function index_to_value(xs::T, index::Integer) where {T<:AbstractAIFloat}
+@inline function index_to_value(aif::T, index::Integer) where {T<:AbstractAIFloat}
     if index < 1 || index > nValues(T)
-        return eltype(floats(xs))(NaN)
+        return eltype(floats(aif))(NaN)
     end
-    floats(xs)[index]
+    floats(aif)[index]
 end
 
 @inline function index_to_value(xs::T, index::Integer) where {T<:Vector{AbstractFloat}}
@@ -175,18 +192,18 @@ end
     xs[index]
 end
 
-@inline function value_to_indexgte(xs::T, x::F) where {T<:AbstractAIFloat, F<:AbstractFloat}
-    findfirst(>=(x), floats(xs))
+@inline function value_to_indexgte(aif::T, x::F) where {T<:AbstractAIFloat, F<:AbstractFloat}
+    findfirst(>=(x), floats(aif))
 end
 
 @inline function value_to_indexgte(xs::T, x::F) where {T<:Vector{<:AbstractFloat}, F<:AbstractFloat}
     findfirst(>=(x), xs)
 end
 
-@inline function value_to_indices(xs::T, x::F) where {T<:AbstractAIFloat, F<:AbstractFloat}
-    hi = value_to_indexgte(xs, x)
+@inline function value_to_indices(aif::T, x::F) where {T<:AbstractAIFloat, F<:AbstractFloat}
+    hi = value_to_indexgte(aif, x)
     isnothing(hi) && return (nothing, nothing)
-    if !isnothing(hi) && x == float(xs)[hi]
+    if !isnothing(hi) && x == float(aif)[hi]
         lo = hi
     else
         lo = hi - 1
@@ -205,16 +222,9 @@ end
     (lo, hi)
 end
 
-@inline function value_to_offset(xs::T, x::F) where {T<:AbstractAIFloat, F<:AbstractFloat}
-    index_to_offset(value_to_index(xs, x))
+@inline function value_to_offset(aif::T, x::F) where {T<:AbstractAIFloat, F<:AbstractFloat}
+    index_to_offset(value_to_index(aif, x))
 end
-
-@inline function value_to_offset(xs::T, x::F) where {T<:Vector{<:AbstractFloat}, F<:AbstractFloat}
-    index_to_offset(value_to_index(xs, x))
-end
-
-offset_to_value(xs::T, ofs::Integer) where {T<:AbstractAIFloat} = index_to_value(xs, offset_to_index(ofs))
-offset_to_value(xs::Vector{<:AbstractFloat}, ofs::Integer) = index_to_value(xs, offset_to_index(ofs))
 
 idxone(::Type{T}) where {T<:AbstractUnsigned} = (((nValues(T) % UInt16) >> 0x0001) + 0x0001)
 idxone(::Type{T}) where {T<:AbstractSigned} = (((nValues(T) % UInt16) >> 0x0002) + 0x0001)
@@ -245,17 +255,20 @@ ofsneginf(::Type{T}) where {T<:AbstractUnsigned} = nothing # throw(DomainError(T
  # cover instantiations
  for F in (:idxone, :idxnegone, :idxnan, :idxinf, :idxneginf,
            :ofsone, :ofsnan, :ofsinf, :ofsneginf)
-    @eval $F(x::T) where {T<:AbstractAIFloat} = $F(T)
+    @eval begin
+        $F(aif::T) where {T<:AbstractAIFloat} = $F(T)
+        # $F(xs::Vector{T}, x::U) where {T<:AbstractFloat, U<:Unsigned} = $F(T)
+    end
  end
 
-is_idxnan(xs::T, idx::U) where {T<:AbstractAIFloat, U<:Unsigned} = idxnan(T) == idx
-is_ofsnan(xs::T, ofs::U) where {T<:AbstractAIFloat, U<:Unsigned} = is_idxnan(xs, offset_to_index(ofs))
+is_idxnan(aif::T, idx::U) where {T<:AbstractAIFloat, U<:Unsigned} = idxnan(T) == idx
+is_ofsnan(aif::T, ofs::U) where {T<:AbstractAIFloat, U<:Unsigned} = is_idxnan(aif, offset_to_index(ofs))
 
-Base.isnan(xs::T, x::U) where {T<:AbstractAIFloat, U<:Unsigned} = is_idxnan(xs, x)
-Base.isnan(xs::T, x::F) where {T<:AbstractAIFloat, F<:AbstractFloat} = isnan(x)
+Base.isnan(aif::T, x::U) where {T<:AbstractAIFloat, U<:Unsigned} = is_idxnan(aif, x)
+Base.isnan(aif::T, x::F) where {T<:AbstractAIFloat, F<:AbstractFloat} = isnan(x)
 
 for F in (:is_idxnan, :is_ofsnan, :isnan)
-    @eval $F(x::T) where {T<:AbstractAIFloat} = $F(T)
+    @eval $F(aif::T) where {T<:AbstractAIFloat} = $F(T)
 end
 
  
